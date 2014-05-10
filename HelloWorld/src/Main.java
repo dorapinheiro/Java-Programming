2f72402a48939ba2c8b3438a9e25e426c74a92b4
Dora Pinheiro <melisand@iol.pt>
2014-05-10 10:23:15 +0100

    solucao com foreach

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -2,20 +2,33 @@
 public class Main {
 
 	public static void main(String[] args) {
-		//System.out.println("Hello World! V2");
-		
-		for(int i=0; i<args.length;i++)
-		{
-			if(i==args.length-1)
+
+	//solucao com foreach	
+	
+		for (String batata : args) {
+			if(batata.equals(args[args.length-1]))
 			{
-				System.out.print( args[i]);
+				System.out.println(batata);
 			}
-			else
-			{
-				System.out.print(args[i] + " ");
+			else{
+			System.out.print(batata + " ");
 			}
 		}
-
 	}
 
 }
+
+
+// usar o for e ter tratamento do último espaço 
+
+//for(int i=0; i<args.length;i++)
+//{
+//	if(i==args.length-1)
+//	{
+//		System.out.print( args[i]);
+//	}
+//	else
+//	{
+//		System.out.print(args[i] + " ");
+//	}
+//}
