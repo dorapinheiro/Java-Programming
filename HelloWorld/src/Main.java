81e35432d228746cc4a1e9dd7eab8104e1a02d86
Dora Pinheiro <melisand@iol.pt>
2014-05-10 09:50:39 +0100

    Hello World em que nao imprime ultimo espaço dos args passados

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -2,7 +2,19 @@
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World! V2");
+		//System.out.println("Hello World! V2");
+		
+		for(int i=0; i<args.length;i++)
+		{
+			if(i==args.length-1)
+			{
+				System.out.print( args[i]);
+			}
+			else
+			{
+				System.out.print(args[i] + " ");
+			}
+		}
 
 	}
 
