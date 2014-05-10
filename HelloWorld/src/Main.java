0c0131a96de5abf01c292c8bcea3142f4c98d5c2
Dora Pinheiro <melisand@iol.pt>
2014-05-10 10:44:55 +0100

    utilizacao de operador ternario

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -3,31 +3,16 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		//solucao com foreach --> nao se deve usar equals numa string porque ele compara o valor. Se passarmos como argumento ola ola, ele vai imprimir olaola	
-
-		for (String batata : args) {
-			if(batata.equals(args[args.length-1]))
-			{
-				System.out.println(batata);
-			}
-			else{
-			System.out.print(batata + " ");
-			}
+		for(int i=0; i<args.length;i++)
+		{
+			// utilização de operador ternario
+			System.out.println(args[i] + (( i== args.length-1)? "\n": " "));
 		}
+
 	}
 }
 
 
-// usar o for e ter tratamento do último espaço 
+// 
 
-//for(int i=0; i<args.length;i++)
-//{
-//	if(i==args.length-1)
-//	{
-//		System.out.print( args[i]);
-//	}
-//	else
-//	{
-//		System.out.print(args[i] + " ");
-//	}
-//}
+
